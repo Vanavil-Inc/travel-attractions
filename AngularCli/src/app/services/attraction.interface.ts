a733823de39272fa84556b0433af2c52519c41cf
Mukesh Vanjara <mukesh.vanjara@vanavil-systems.com>
2018-08-06 14:56:54 +0530

    delete all files

--- AngularCli/src/app/services/attraction.interface.ts
+++
@@ -1,3 +0,0 @@
-export interface Attraction{
-  title: String;
-}
--- AngularCli/src/app/user/validators/index.ts
+++
@@ -1,3 +0,0 @@
-export { PasswordValidator } from './password.validator';
-export { PhoneValidator } from './phone.validator';
-export { Country } from './country.model'
